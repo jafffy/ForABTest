3ee3203a093559cc18d671f6b898b817f3245cfd
Jaewon Choi <jainersoer@ajou.ac.kr>
2016-10-06 22:35:53 +0900

    Add subjectInfo

--- app/surveyForm/surveyForm.js
+++ app/surveyForm/surveyForm.js
@@ -12,7 +12,28 @@ angular.module('myApp.surveyForm', ['ngRoute'])
 	});
 }])
 
-.controller('SurveyFormCtrl', ["$scope", function($scope) {
+.controller('SurveyFormCtrl',
+	["$scope", "subjectInfo",
+		function($scope, subjectInfo) {
+	$scope.subjectID = "";
 	$scope.type = "A";
 	$scope.gender = "F";
+
+	$scope.passSubjectInfo = function() {
+		subjectInfo.setSubjectID($scope.subjectID);
+		subjectInfo.setType($scope.type);
+		subjectInfo.setGender($scope.gender);
+	}
+}])
+
+.factory('subjectInfo', [function() {
+	var subjectID = -1;
+	var type = "A";
+	var gender = "F";
+
+	return {
+		setSubjectID: function(sid) { subjectID = sid; },
+		setType: function(type_) { type = type_; },
+		setGender: function(gender_) { gender = gender_; }
+	}
 }]);
