c525cd87fb16f5b5a483092a1266e2b45c891bfd
Jaewon Choi <jaewon.james.choi@gmail.com>
2016-10-07 21:43:41 +0900

    Add buttons

--- app/app.js
+++ app/app.js
@@ -7,6 +7,7 @@ angular.module('myApp', [
   'myApp.descriptionForm',
   'myApp.view1',
   'myApp.view2',
+  'myApp.wordMatchingView',
   'myApp.version'
 ]).
 config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
