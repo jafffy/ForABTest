0ab7aaebdaa9242afd0855840e255dfd98dcc093
Jaewon Choi <jaewon.james.choi@gmail.com>
2016-10-09 08:26:38 +0900

    Select colorful cards

--- app/wordMatchingView/wordMatchingView.js
+++ app/wordMatchingView/wordMatchingView.js
@@ -18,6 +18,8 @@ app.config(['$routeProvider', function($routeProvider) {
     function($scope, $location, dataService) {
   $scope.words = null;
   $scope.cards = [];
+	var count = 0;
+
   dataService.getWords().then(function(dataResponse) {
 	  var data = JSON.parse('{' + dataResponse.data + '}');
     $scope.words = data['words'];
@@ -30,8 +32,9 @@ app.config(['$routeProvider', function($routeProvider) {
         }
         card = [];
       }
-
-      card.push({name: $scope.words[i]});
+			var c = {name: $scope.words[i], isClicked: false, color: "#C8C8C8",
+      fontColor: "#898989"};
+      card.push(c);
     }
 
     $scope.cards.push(card);
@@ -39,33 +42,34 @@ app.config(['$routeProvider', function($routeProvider) {
 
 	var colorList = ["#4368BA", "#AFAF1E", "#FA605B", "#2FD1B2", "#FCC106"]
 
-	$scope.color = ["", "#C8C8C8", "#C8C8C8", "#C8C8C8", "#C8C8C8", "#C8C8C8"];
-  $scope.fontColor = ["", "#898989", "#898989", "#898989", "#898989", "#898989"];
-	$scope.isClicked = [false, false, false, false, false, false];
-
-  $scope.setColor = function(i) {
-    if ($scope.isClicked[i])
+  $scope.setColor = function(c) {
+    if (c.isClicked)
       return;
-    $scope.isClicked[i] = true;
+
+	  if (count >= 5) {
+	    return;
+    }
+    count += 1;
+    c.isClicked = true;
 
     var colorPicker = Math.floor(Math.random() * 5);
 
-    $scope.color[i] = colorList[colorPicker]; // "#4368BA";
-    $scope.fontColor[i] = "#fff";
+    c.color = colorList[colorPicker];
+    c.fontColor = "#fff";
   };
-  $scope.setHoverColor = function(i) {
-    if ($scope.isClicked[i]) {
+  $scope.setHoverColor = function(c) {
+    if (c.isClicked) {
       return;
     }
-    $scope.color[i] = "#898989";
-    $scope.fontColor[i] = "#fff";
+    c.color = "#898989";
+    c.fontColor = "#fff";
   };
-  $scope.setLeaveColor = function(i) {
-    if ($scope.isClicked[i]) {
+  $scope.setLeaveColor = function(c) {
+    if (c.isClicked) {
       return;
     }
-    $scope.color[i] = "#C8C8C8";
-    $scope.fontColor[i] = "#898989";
+    c.color = "#C8C8C8";
+    c.fontColor = "#898989";
   };
 
   $scope.nextPage = function() {
