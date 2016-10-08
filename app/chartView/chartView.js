ad4558f70f048e5dfbc8e9e7b473d57ef251e2ed
Jaewon Choi <jaewon.james.choi@gmail.com>
2016-10-09 05:42:39 +0900

    Add simple get

--- app/chartView/chartView.js
+++ app/chartView/chartView.js
@@ -1,15 +1,34 @@
 'use strict';
 
-angular.module('myApp.chartView', ['ngRoute', 'chart.js'])
+var app = angular.module('myApp.chartView', ['ngRoute', 'chart.js']);
 
-.config(['$routeProvider', function($routeProvider) {
+
+app.service('dataService', function($http) {
+	delete $http.defaults.headers.common['X-Requested-With'];
+	this.getData = function() {
+		return $http({
+			method: 'GET',
+			url: 'http://localhost:3000/users?userID=1',
+		});
+	}
+});
+
+app.config(['$routeProvider', function($routeProvider) {
 	$routeProvider.when('/chartView', {
 		templateUrl: 'chartView/chartView.html',
 		controller: 'ChartViewCtrl'
 	});
 }])
 
-.controller('ChartViewCtrl', ['$scope', '$location', function($scope, $location) {
+.controller('ChartViewCtrl',
+	['$scope', '$location', 'dataService',
+		function($scope, $location, dataService) {
+	$scope.responseFromServer = null;
+  dataService.getData().then(function(dataResponse) {
+		$scope.responseFromServer = dataResponse;
+	  console.log($scope.responseFromServer);
+  });
+
 	$scope.series = ['Series A', 'Series B'];
 	$scope.labels = ["날짜 1", "날짜 2", "날짜 3", "날짜 4"];
 	$scope.data = [
