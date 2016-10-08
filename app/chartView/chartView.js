abe4604fa820dd9cd183e7dc2b2c5fcb1c4cea17
Jaewon Choi <jaewon.james.choi@gmail.com>
2016-10-09 05:12:02 +0900

    Add chart legend

--- app/chartView/chartView.js
+++ app/chartView/chartView.js
@@ -11,15 +11,19 @@ angular.module('myApp.chartView', ['ngRoute', 'chart.js'])
 
 .controller('ChartViewCtrl', ['$scope', '$location', function($scope, $location) {
 	$scope.series = ['Series A', 'Series B'];
+	$scope.labels = ["날짜 1", "날짜 2", "날짜 3", "날짜 4"];
 	$scope.data = [
-		[ {x: 0, y: 10}, {x: 1, y: 1}, {x: 2, y: 6}, {x: 5, y: 2} ],
-		[ {x: 0, y: 2}, {x: 2, y: 7}, {x: 4, y: 2}, {x: 5, y: 9}]
+		[3, 4, 5, 6],
+		[5, 3, 4, 2]
 	];
+	$scope.datasetOverride = [{ yAxisID: 'y-axis-1' }];
 	$scope.options = {
 		scales: {
-			xAxes: [{
+			yAxes: [{
+				id: 'y-axis-1',
 				type: 'linear',
-				position: 'bottom'
+				display: true,
+				position: 'left'
 			}]
 		}
 	};
