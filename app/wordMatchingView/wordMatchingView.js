604d91ae7ce0ae2c20f18121f25d85fca0c19a64
Jaewon Choi <jaewon.james.choi@gmail.com>
2016-10-09 03:34:16 +0900

    Set random color to wordCards

--- app/wordMatchingView/wordMatchingView.js
+++ app/wordMatchingView/wordMatchingView.js
@@ -13,6 +13,35 @@ angular.module('myApp.wordMatchingView', ['ngRoute'])
   });
 }])
 
-.controller('WordMatchingViewCtrl', [function() {
+.controller('WordMatchingViewCtrl', ["$scope", function($scope) {
+	var colorList = ["#4368BA", "#AFAF1E", "#FA605B", "#2FD1B2", "#FCC106"]
 
+	$scope.color = ["", "#C8C8C8", "#C8C8C8", "#C8C8C8", "#C8C8C8", "#C8C8C8"];
+  $scope.fontColor = ["", "#898989", "#898989", "#898989", "#898989", "#898989"];
+	$scope.isClicked = [false, false, false, false, false, false];
+
+  $scope.setColor = function(i) {
+    if ($scope.isClicked[i])
+      return;
+    $scope.isClicked[i] = true;
+
+    var colorPicker = Math.floor(Math.random() * 5);
+
+    $scope.color[i] = colorList[colorPicker]; // "#4368BA";
+    $scope.fontColor[i] = "#fff";
+  };
+  $scope.setHoverColor = function(i) {
+    if ($scope.isClicked[i]) {
+      return;
+    }
+    $scope.color[i] = "#898989";
+    $scope.fontColor[i] = "#fff";
+  };
+  $scope.setLeaveColor = function(i) {
+    if ($scope.isClicked[i]) {
+      return;
+    }
+    $scope.color[i] = "#C8C8C8";
+    $scope.fontColor[i] = "#898989";
+  };
 }]);
